4b754b817e104f7c1fe5691758c8b08fe2ae7919
KevinFarthing <39893734+KevinFarthing@users.noreply.github.com>
2018-07-27 10:48:05 -0500

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,5 @@ firebase.initializeApp(config);
 // var ref = firebase.database().ref('players');
 // // var playersRef = firebase.database().ref("players/");
 
+var weatherMap = "d630e599f0fe8509f503266e4aaa10de"
+var barChart = "fa47da496ca8f216e6cbd7fa0dc9d818"
