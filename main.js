60a714908b921847d0e2597b625439cad2ce9ec0
KevinFarthing <farthing.kw@gmail.com>
2018-07-27 10:35:30 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+var config = {
+    apiKey: "AIzaSyDjjxmTSePqUavKuLuWmZc-K_tKukvI_Uw",
+    // authDomain: "<PROJECT_ID>.firebaseapp.com",
+    databaseURL: "https://github.com/KevinFarthing/firebase_test.git" //,
+    // projectId: "<PROJECT_ID>",
+    // storageBucket: "<BUCKET>.appspot.com",
+    // messagingSenderId: "<SENDER_ID>",
+};
+firebase.initializeApp(config);
+
+// console.log(firebase);
+
+// var ref = firebase.database().ref('players');
+// // var playersRef = firebase.database().ref("players/");
+
